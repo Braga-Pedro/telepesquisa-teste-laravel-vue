5d7f85b079e63eeb662aeceda2d3ff74dafd89e2
Pedro <pedro.projetos.prog@gmail.com>
2025-06-01 01:13:02 -0300

    add fields

--- database/migrations/2025_05_30_221330_create_empresas_table.php
+++ database/migrations/2025_05_30_221330_create_empresas_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('empresas', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('cep');
+            $table->string('rua');
+            $table->string('numero_casa');
+            $table->string('bairro');
+            $table->string('estado');
+            $table->string('telefone');
+            $table->string('cpf_cnpj')->unique();
+
+            $table->unsignedBigInteger('segmento_id');
+            $table->foreign('segmento_id')->references('id')->on('segmentos');
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
