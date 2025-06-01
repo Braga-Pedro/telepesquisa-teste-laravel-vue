8d86dd4e5bef566b91c17ed88dc9ac24375d0aa5
Pedro <pedro.projetos.prog@gmail.com>
2025-06-01 01:11:58 -0300

    update store and index

--- app/Http/Controllers/Empresas/EmpresaController.php
+++ app/Http/Controllers/Empresas/EmpresaController.php
@@ -1,9 +1,16 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Empresas;
 
 use App\Models\Empresa;
+use App\Models\Segmento;
+use App\Http\Requests\Empresas\StoreEmpresaRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use Exception;
+use Inertia\Inertia;
 
 class EmpresaController extends Controller
 {
@@ -12,7 +19,19 @@ class EmpresaController extends Controller
      */
     public function index()
     {
-        //
+        $empresas = Empresa::whereNull('deleted_at')
+            ->orderBy('updated_at', 'desc')
+            ->with('segmento')
+            ->get();
+
+        $segmentos = Segmento::whereNull('deleted_at')
+            ->orderBy('nome', 'desc')
+            ->get();
+
+        return Inertia::render('Empresas/Index', [
+            'empresas' => $empresas,
+            'segmentos' => $segmentos
+        ]);
     }
 
     /**
@@ -26,9 +45,60 @@ class EmpresaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreEmpresaRequest $request)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $validatedData = $request->validated();
+
+            $validatedData = $this->validateSegmento($validatedData);
+
+            Empresa::create($validatedData);
+
+            DB::commit();
+
+            $segmentos = Segmento::whereNull('deleted_at')
+                ->orderBy('nome', 'desc')
+                ->get();
+
+            return redirect()->route('empresas.index')->with('flash', [
+                'type' => 'success',
+                'message' => 'Empresa criada com sucesso!',
+                'segmentos' => $segmentos,
+            ]);
+        } catch (\Exception $exception) {
+            DB::rollBack();
+
+            $message_error = 'Erro ao criar empresa: ' . $exception->getMessage();
+            Log::error($message_error);
+
+            return redirect()->route('empresas.index')->with('flash', [
+                'type' => 'error',
+                'message' => $message_error,
+            ]);
+        }
+    }
+
+    private function validateSegmento($validatedData)
+    {
+        if (empty($validatedData['segmento_id']) && empty($validatedData['segmento'])) {
+            return redirect()->route('empresas.index')->with('flash', [
+                'type' => 'error',
+                'message' => 'É necessário informar o segmento.',
+            ]);
+        }
+
+        if (!empty($validatedData['segmento_id'])) {
+            $segmento = Segmento::find($validatedData['segmento_id']);
+            if (!$segmento) {
+                throw new Exception("Segmento não encontrado.");
+            }
+        } elseif (!empty($validatedData['segmento']) && empty($validatedData['segmento_id'])) {
+            $segmento = Segmento::firstOrCreate(
+                ['nome' => $validatedData['segmento']],
+            );
+            $validatedData['segmento_id'] = $segmento->id;
+        }
     }
 
     /**
@@ -62,4 +132,5 @@ class EmpresaController extends Controller
     {
         //
     }
+
 }
